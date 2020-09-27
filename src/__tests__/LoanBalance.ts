7de31927a35fc8087333bf73f3a836f050053d0c
Alejandro Paz <alepazte@hotmail.com>
2020-09-27 17:15:44 -0600

    feat: add function to calculate remaining loan balance

---
+++ src/__tests__/LoanBalance.ts
@@ -0,0 +1,15 @@
+import { getLoanBalance } from '../index';
+test('Loan Balance', () => {
+  expect(getLoanBalance(2e5, 7.5e-2, 30 * 12, 12)).toBe(198156.33114503196);
+  expect(getLoanBalance(2e5, 7.5e-2, 30 * 12, 0)).toBe(2e5);
+  expect(() => getLoanBalance(-2e5, 7.5e-2, 30 * 12, 12)).toThrow('Please provide a valid loan');
+  expect(() => getLoanBalance(2e5, -7.5e-2, 30 * 12, 12)).toThrow('Please provide a valid interest rate');
+  expect(() => getLoanBalance(2e5, 7.5e-2, -30 * 12, 12)).toThrow('Please provide a valid loan length');
+  expect(() => getLoanBalance(2e5, 7.5e-2, -30 * 12, -12)).toThrow('Please provide a valid number of Period of elapsed months');
+  expect(() => getLoanBalance(2e5, 7.5e-2, 30 * 12, 12, 0)).toThrow(
+    'Please provide a valid number of times interest compounds',
+  );
+  expect(() => getLoanBalance(2e5, 7.5e-2, 30 * 12, 12, -10)).toThrow(
+    'Please provide a valid number of times interest compounds',
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -28,3 +28,36 @@ export const getMonthlyPayment = (
   const interest = interestRate / timesInterestCompounds;
   return loan * ((interest * Math.pow(1 + interest, loanLength)) / (Math.pow(1 + interest, loanLength) - 1));
 };
+
+/*
+ * Loan Balance
+ *
+ * Function to calculate the remaining loan balance of a fixed payment loan after a period of months
+ *
+ * B = L[(1 + c)n - (1 + c)p]/[(1 + c)n - 1]
+ *
+ * B = L[(1 + c/t)^n - (1 + c/t)^p]/[(1 + c/t)^n - 1]
+ *
+ * @param {number} Loan Amount (L)
+ * @param {number} Interest rate annually (c)
+ * @param {number} Loan length express in months (n)
+ * @param {number} Period of elapsed months (p)
+ * @param {number} Number of times interest compounds, default 12 (t)
+ * @returns {number} Monthly Payment
+ */
+
+export const getLoanBalance = (
+  loan: number,
+  interestRate: number,
+  loanLength: number,
+  periodElapsedMonths: number,
+  timesInterestCompounds: number = 12,
+) => {
+  if (loan < 0) throw new Error('Please provide a valid loan');
+  if (interestRate < 0) throw new Error('Please provide a valid interest rate');
+  if (loanLength < 0) throw new Error('Please provide a valid loan length');
+  if (timesInterestCompounds <= 0) throw new Error('Please provide a valid number of times interest compounds');
+  if (periodElapsedMonths < 0) throw new Error('Please provide a valid number of Period of elapsed months');
+  const interest = interestRate / timesInterestCompounds;
+  return loan * ((Math.pow(1 + interest, loanLength) - Math.pow(1 + interest, periodElapsedMonths)) / (Math.pow(1 + interest, loanLength) - 1));
+};
